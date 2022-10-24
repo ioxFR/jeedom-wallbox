dbdcb19cb50ddbc33bcaab4d68cae7630c63e780
Valentin LECERF <valentin.lecerf@vlecerf.com>
2022-10-24 06:27:35 +0200

    added exception to configuration page

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -26,7 +26,9 @@ $password = config::byKey("password", "wallbox");
 if($username == null || $password == null){
 ?>
 <p style="color:red;text-align: center;font-weight: bold;">{{Username & Password are required for plugin.}}</p>
-<?php } ?>
+<?php 
+throw new Exception("A username and password is required for the plugin to work")
+} ?>
 <form class="form-horizontal">
   <fieldset>
     <div class="form-group">
