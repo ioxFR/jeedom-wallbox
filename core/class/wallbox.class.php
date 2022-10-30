8e725060d3b17699e638aec3c302e62509d2c806
Valentin LECERF <valentin.lecerf@vlecerf.com>
2022-10-30 07:03:00 +0100

    Fix for max amp

--- core/class/wallbox.class.php
+++ core/class/wallbox.class.php
@@ -773,12 +773,12 @@ class wallboxCmd extends cmd {
       }
       else if($this->getLogicalId() == 'maxpower')
       {
+         $inputValue = $_options['slider'];
          log::add('wallbox', 'info', 'execute maxpower');
-         log::add('wallbox','debug',$_options['slider']);
          $obj = $eqlogic->getCmd(null, 'maxpower');
-         log::add('wallbox', 'info', 'setting value to'.$obj->getValue());
+         log::add('wallbox', 'info', 'setting value to'.$inputValue);
          // we change max amp value
-         $this->getEqLogic()->defineMaxAmp($obj->getValue());
+         $this->getEqLogic()->defineMaxAmp($inputValue);
 
       }
    }
